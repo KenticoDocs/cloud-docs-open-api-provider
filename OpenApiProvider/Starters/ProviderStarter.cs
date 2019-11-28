d062aee98412f7ef5f366a3d9d92ca7424e1951b
Enngage <ennspassion@gmail.com>
2019-11-28 12:00:55 +0100

    test: skips preprocessor activity and return

--- OpenApiProvider/Starters/ProviderStarter.cs
+++ OpenApiProvider/Starters/ProviderStarter.cs
@@ -67,7 +67,6 @@ namespace OpenApiProvider.Starters
                     };
 
                 default:
-                    log.Log(LogLevel.Trace, $"Starting new orchestration. Status: {orchestratorInstance?.RuntimeStatus}. Id: {orchestratorInstance?.InstanceId}. Name: {orchestratorInstance?.Name}");
                     return await starter.RunOrchestrator(
                         req,
                         apiReference,
